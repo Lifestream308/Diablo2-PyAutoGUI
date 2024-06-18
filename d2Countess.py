9d85a59872af6de709976f653801e08b3c78dfe5
Christian Arroyo <ChristianArroyo308@yahoo.com>
2024-06-17 22:15:03 -0700

    now reaching level 5 and killing countess

--- d2Countess.py
+++ d2Countess.py
@@ -88,14 +88,14 @@ time.sleep(0.27)
 pyautogui.rightClick(x=1850, y=850)
 
 # now inside tower ruins, move mouse to enter the ruins by clicking
-pyautogui.moveTo(x=927, y=635, duration=1)
+pyautogui.moveTo(x=927, y=635, duration=.5)
 pyautogui.click()
 
-time.sleep(2)
+time.sleep(1.5)
 pyautogui.press('Esc')
 
 # Check image to see if inside the safe part of the ruins. Unpause if safe
-image_path = 'diablo2Images/safeRuins.jpg'
+image_path = 'diablo2Images/safeRuins2.jpg'
 region = (0, 0, screen_width // 2, screen_height)
 location = pyautogui.locateOnScreen(image_path, confidence=0.7, region=region)
 
@@ -105,12 +105,12 @@ if location:
 else:
     print("Printing out error: Safe Ruins image not found.")
 
-time.sleep(1)
+time.sleep(.5)
 
 # click on entrance to ruins officially
-pyautogui.moveTo(545, 543, duration=1)
+pyautogui.moveTo(545, 543, duration=.5)
 pyautogui.click()
-time.sleep(1)
+time.sleep(.75)
 
 # right click teleport to right side of screen closer to level 2
 pyautogui.rightClick(x=1919, y=1000)
@@ -149,7 +149,7 @@ try:
     location = pyautogui.locateOnScreen(image_path, confidence=0.7, region=region)
     if location:
         print(f"Ruins level 2 ENTRANCE image found at: {location}")
-    pyautogui.moveTo(location.left + 75, location.top + 125, duration=0.6)
+    pyautogui.moveTo(location.left + 75, location.top + 100, duration=0.6)
     pyautogui.click()
 
 except Exception as e:
@@ -175,7 +175,7 @@ time.sleep(1)
 # left click to walk towards wall
 pyautogui.click(50, 900)
 
-time.sleep(2)
+time.sleep(1.7)
 
 # right click to teleport over wall and towards level 3
 pyautogui.rightClick(30,950)
@@ -183,21 +183,21 @@ time.sleep(.27)
 pyautogui.rightClick(30,950)
 
 # left click to enter ruins level 3 
-pyautogui.moveTo(488, 329, duration=0.75)
+pyautogui.moveTo(470, 329, duration=0.75)
 
 pyautogui.click()
 
 time.sleep(1)
 
-# right click teleport 3 times to bottom left
-pyautogui.rightClick(10,1010)
+# right click teleport 3 times to bottom left. Originally 10, 1010 for X and Y but running into barrels getting stuck
+pyautogui.rightClick(20,1010)
 time.sleep(.27)
-pyautogui.rightClick(10,1010)
+pyautogui.rightClick(20,1010)
 time.sleep(.27)
-pyautogui.rightClick(10,1010)
-time.sleep(.27)
+pyautogui.rightClick(20,1010)
 
 # 3 times towards right to reach level 4
+pyautogui.moveTo(1850, 905, duration=.3)
 pyautogui.rightClick(1850, 905)
 time.sleep(.27)
 pyautogui.rightClick(1850, 905)
@@ -235,14 +235,124 @@ if location:
 else:
     print("Printing out error: Safe Ruins image not found.")
 
-# teleport to reach entrance to level 5 
+time.sleep(.5)
 
-# enter level 5 
+# teleport towards left to get closer to level 5 entrance
+pyautogui.rightClick(0, 215)
+time.sleep(.27)
+pyautogui.rightClick(0, 215)
+time.sleep(.27)
+pyautogui.rightClick(0, 215)
+time.sleep(.27)
+
+# teleport bottom left to reach level 5 entrance
+pyautogui.rightClick(90, 860)
+time.sleep(.27)
+pyautogui.rightClick(90, 860)
+time.sleep(.27)
+pyautogui.rightClick(90, 860)
+time.sleep(.27)
+pyautogui.rightClick(90, 860)
+
+time.sleep(1)
+
+# try to find image of level 5 entrance then click to enter 
+try:
+    image_path = 'diablo2Images/ruinsLevel5Entrance2.jpg'
+    region = (0, 0, screen_width, screen_height)
+    location = pyautogui.locateOnScreen(image_path, confidence=0.7, region=region)
+    if location:
+        print(f"Ruins level 5 ENTRANCE image found at: {location}")
+    pyautogui.moveTo(location.left + 125, location.top + 150, duration=0.6)
+    pyautogui.click()
+
+except Exception as e:
+    print(f"Error. Can't find level 5 Entrance: {e}")
+
+time.sleep(.5)
+
+pyautogui.press('Esc')
+
+time.sleep(.5)
+
+# check that I am inside level 5 then unpause game
+image_path = 'diablo2Images/ruinsLevel52.jpg'
+region = (screen_width // 2, 0, screen_width // 2, screen_height)
+location = pyautogui.locateOnScreen(image_path, confidence=0.7, region=region)
+
+if location:
+    print(f"Ruins level 5 image found at: {location}")
+    pyautogui.press('Esc')
+else:
+    print("Printing out error: Ruins level 5 image not found.")
 
 # teleport to countess
+time.sleep(.27)
+pyautogui.rightClick(1815, 148)
+time.sleep(.27)
+pyautogui.rightClick(1815, 148)
+time.sleep(.27)
+pyautogui.rightClick(1815, 148)
+time.sleep(.27)
+
+# pyautogui.press('Esc')
 
 # cast Poison Nova
+pyautogui.press('F4')
+time.sleep(.1)
+pyautogui.rightClick()
+
+# cast Decrepify
+time.sleep(.1)
+pyautogui.press('F8')
+time.sleep(.3)
+pyautogui.rightClick(1262, 413)
+time.sleep(.3)
+pyautogui.rightClick(964, 338)
+time.sleep(.3)
+pyautogui.rightClick(793, 443)
+time.sleep(.3)
+
+# cast Poison Nova
+pyautogui.press('F4')
+time.sleep(.1)
+pyautogui.rightClick()
+
+# cast Decrepify
+time.sleep(.1)
+pyautogui.press('F8')
+time.sleep(.3)
+pyautogui.rightClick(1262, 413)
+time.sleep(.3)
+pyautogui.rightClick(964, 338)
+time.sleep(.3)
+pyautogui.rightClick(793, 443)
+time.sleep(.3)
+
+# cast Poison Nova
+pyautogui.press('F4')
+time.sleep(.1)
+pyautogui.rightClick()
 
 # cast Corpse Explosion 
+time.sleep(.1)
+pyautogui.press('F2')
+time.sleep(.3)
+pyautogui.rightClick(964, 338)
+time.sleep(.3)
+pyautogui.rightClick(964, 338)
+time.sleep(.3)
+pyautogui.rightClick(964, 338)
+time.sleep(.3)
+pyautogui.rightClick(964, 338)
+time.sleep(.3)
+pyautogui.rightClick(964, 338)
+
+time.sleep(.5)
 
 # look for loot
+pyautogui.press('alt')
+
+time.sleep(3)
+
+pyautogui.press('esc')
